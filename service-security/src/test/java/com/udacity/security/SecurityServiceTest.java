8500b0ae4d78c0590e1e4fed709bfa7dbb4489fb
Andrey Shulga <ray7sun@gmail.com>
2021-12-14 09:07:09 +0300

    update: SecurityServiceTest, Application Requirements - 1

--- service-security/src/test/java/com/udacity/security/SecurityServiceTest.java
+++ service-security/src/test/java/com/udacity/security/SecurityServiceTest.java
@@ -1,25 +1,28 @@
 package com.udacity.security;
 
 import com.udacity.image.ImageServiceHelper;
-import com.udacity.security.data.SecurityRepository;
-import com.udacity.security.data.Sensor;
-import com.udacity.security.data.SensorType;
+import com.udacity.security.data.*;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class SecurityServiceTest {
 
     private Sensor sensor;
     private SecurityService securityService;
-    private final String randomUID = UUID.randomUUID().toString();
 
     @Mock
     SecurityRepository securityRepository;
@@ -29,16 +32,16 @@ public class SecurityServiceTest {
     @BeforeEach
     void settingUp() {
         securityService = new SecurityService(securityRepository, imageServiceHelper);
-        sensor = new Sensor(randomUID, SensorType.DOOR);
+        sensor = new Sensor(UUID.randomUUID().toString(), SensorType.DOOR);
     }
 
-
-
-
-
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    //Application Requirements - 1
+    @ParameterizedTest
+    @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
+    public void alarmStatusChanging_ifAlarmIsArmedAndSensorActivated_alarmStatusPending (ArmingStatus status) {
+        when(securityService.getArmingStatus()).thenReturn(status);
+        when(securityService.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
+        securityService.changeSensorActivationStatus(sensor, true);
+        verify(securityRepository, Mockito.times(1)).setAlarmStatus(AlarmStatus.PENDING_ALARM);
     }
 }
