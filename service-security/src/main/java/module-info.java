73d03067b2b99811b8804ac193aeb288b8c86195
Andrey Shulga <ray7sun@gmail.com>
2021-12-20 15:39:05 +0300

    update: module-info.java

--- service-security/src/main/java/module-info.java
+++ service-security/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.udacity.security {
+    opens com.udacity.security.data to com.google.gson;
     requires com.udacity.image;
     requires java.desktop;
     requires miglayout;
     requires com.google.gson;
     requires com.google.common;
     requires java.prefs;
-    opens com.udacity.security.data to com.google.gson;
 
 }
